baada42e1e62ed6fa20c9b59bdfdc1095052bf96
Ancora-SIRlab <1149771519@qq.com>
2017-03-26 17:02:00 +1100

    data collection scripts

---
+++ blind_grasp_server.py
@@ -0,0 +1,219 @@
+import socket
+
+import os, sys
+import csv
+import time
+from image_saver2 import ImageSaver
+
+#num of batch
+#batch_num = 0
+
+#num of grasp attempt per grasp batch
+#batch_size = 40
+
+#port and ip address
+ip_port = ('192.168.0.116',8892)
+
+#maximum connection number
+connect_num = 100
+
+#data folder
+#data_path = './new_data/'
+data_path = './' + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + '/'
+os.mkdir(data_path)
+print '********************************************'
+print 'data path: ' + data_path
+print '********************************************'
+
+#receive for every socket communication
+#blind grasp data collection
+def receive_from_robot(conn, iteration, confirm, fcsv, headers, img_saver):
+
+	'''
+	args:
+		conn:		socket
+		iteration:	num of grasp attempt in this batch
+		confirm: 	message type from robot controller
+		fcsv:		output data csv file		
+		headers: 	csv file headers
+		ima_saver:	image save class, include two cameras
+
+	return: none
+	'''
+
+
+
+	'''
+	grasp stracture:
+	
+	***first random move	ITF-SMS0
+	shot 00 	no gripper
+	move 00		first random move
+	shot 01		shot first random move
+
+	***second random move	ITF-MoSo
+	move 1		second random move
+	shot 1		shot second random move
+
+	***grasp and drop	ITF-Pick
+	pick
+	shot 11		shot the pick
+	move 		to drop box
+	shot 12		shot the up position
+	drop		drop the toy
+	shot 13 	shot the binose the gri
+
+	'''
+
+	#shot 00 (not use, same as shot 13) 
+	if confirm == 'shot_00':			
+		img_saver.kinect_saver(data_path + 'cam1_I_' + str(iteration) + '_00')
+		#img_saver.lifcam_saver('cam2_I_' + str(iteration) + '_00.png')
+		conn.send(bytes(confirm))
+		print confirm
+
+
+
+	
+	#shot 01 (used)	
+	elif confirm == 'shot_01':
+		print '***ITF-SMS0***********************************'		
+		img_saver.kinect_saver(data_path + 'cam1_I_' + str(iteration) + '_01')
+		#img_saver.lifcam_saver('cam2_I_' + str(iteration) + '_01.png')
+		conn.send(bytes(confirm))
+		print 'complete ' + confirm
+		print '**********************************************'
+
+	
+	#shot 1	(uesd)	
+	elif confirm == 'shot_1':
+		print '***ITF-MoSo***********************************'		
+		img_saver.kinect_saver(data_path + 'cam1_I_' + str(iteration) + '_1')
+		#img_saver.lifcam_saver('cam2_I_' + str(iteration) + '_1.png')
+		conn.send(bytes(confirm))
+		print 'complete ' + confirm
+		print '**********************************************'
+
+
+	
+	#shot 11 (used)	
+	elif confirm == 'shot_11':	
+		print '***ITF-Pick***********************************'	
+		img_saver.kinect_saver(data_path + 'cam1_I_' + str(iteration) + '_11')
+		#img_saver.lifcam_saver('cam2_I_' + str(iteration) + '_11.png')
+		conn.send(bytes(confirm))
+		print 'complete ' + confirm
+		print '**********************************************'
+
+
+	
+	#shot 12 (used)	
+	elif confirm == 'shot_12':
+		img_saver.kinect_saver(data_path + 'cam1_I_' + str(iteration) + '_12')
+		#img_saver.lifcam_saver('cam2_I_' + str(iteration) + '_12.png')
+		conn.send(bytes(confirm))
+		print 'complete ' + confirm
+		print '**********************************************'
+	
+
+
+	
+	#shot 13 (used)	
+	elif confirm == 'shot_13':
+		img_saver.kinect_saver(data_path + 'cam1_I_' + str(iteration) + '_13')
+		#img_saver.lifcam_saver('cam2_I_' + str(iteration) + '_13.png')
+		conn.send(bytes(confirm))
+		print 'complete ' + confirm
+		print '**********************************************'
+
+	
+	#move 00 (used)
+	#move 1	(used):	
+	elif confirm == 'data':
+
+		#move 00
+		conn.send(bytes('move_00'))
+		random_c_0 = conn.recv(1024)
+
+		#move 1
+		conn.send(bytes('move_1'))
+		random_c_1 = conn.recv(1024)
+
+		#rotate_angle
+		conn.send(bytes('rotate_angle'))
+		rotate_angle = conn.recv(1024)
+		rows = [{'move_00': random_c_0, 'move_1': random_c_1, 'rotate_angle': rotate_angle}]
+	
+		#save data as csv file
+		with open(data_path + 'data.csv', 'a') as f:
+			fcsv = csv.DictWriter(f, headers)
+			fcsv.writerows(rows)
+
+		print 'complete ' + confirm
+		print '**********************************************'
+
+	#error
+	else:
+		print(confirm)
+		print 'error'
+		print '**********************************************'
+
+
+
+
+#connect to robot and communicate with socket
+def connect_robot(ip_port, connect_num):
+
+	'''
+	args:	
+		ip_port:	ip address and port
+		connect_num:	maximun connection number 
+
+	return: none
+	'''
+
+	#open the socket server, waiting for data	
+	sock = socket.socket()
+	sock.bind(ip_port)
+	sock.listen(connect_num)
+	img_saver = ImageSaver()
+	print 'connecting the robot'
+	print 'waiting for data'
+	print '******************************************'
+	
+	#grasp cycle number
+	iteration = 0
+
+	#save data in csv file
+	# headers of csv 
+	headers = ['move_00', 'move_1', 'rotate_angle']
+	# create csv file
+	with open(data_path + 'data.csv', 'w') as f:
+		fcsv = csv.DictWriter(f, headers)
+		fcsv.writeheader()
+
+	#start to recieve data
+	while True:
+		iteration += 1
+		
+		for i in range(6):
+			conn,addr = sock.accept()
+			confirm = conn.recv(1024)
+			receive_from_robot(conn,iteration, confirm, fcsv, headers, img_saver)
+
+
+		
+
+#main function
+def main(args):
+
+	#connect
+	connect_robot(ip_port, connect_num)
+
+
+if __name__ == '__main__':
+	main(sys.argv)
+
+
+
+
---
+++ image_saver2.py
@@ -0,0 +1,86 @@
+# coding: utf-8
+# author: Xiaoyi He
+import numpy as np
+import cv2
+import sys
+from pylibfreenect2 import Freenect2, SyncMultiFrameListener
+from pylibfreenect2 import FrameType, Registration, Frame
+from pylibfreenect2 import createConsoleLogger, setGlobalLogger
+from pylibfreenect2 import LoggerLevel
+
+class ImageSaver(object):
+	"""a class for image saving"""
+	def __init__(self):
+		try:
+		    from pylibfreenect2 import OpenCLPacketPipeline
+		    self.pipeline = OpenCLPacketPipeline()
+		except:
+		    from pylibfreenect2 import CpuPacketPipeline
+		    self.pipeline = CpuPacketPipeline()
+
+		self.fn = Freenect2()
+		num_devices = self.fn.enumerateDevices()
+		if num_devices == 0:
+		    print("No device connected!")
+		    return
+
+		self.serial = self.fn.getDeviceSerialNumber(0)
+		self.device = self.fn.openDevice(self.serial, pipeline=self.pipeline)
+
+		self.listener = SyncMultiFrameListener(
+		    FrameType.Color | FrameType.Ir | FrameType.Depth)
+		# Register listeners
+		self.device.setColorFrameListener(self.listener)
+		self.device.setIrAndDepthFrameListener(self.listener)
+
+		self.device.start()
+		self.cap = cv2.VideoCapture(0)
+
+	def __del__(self):
+		self.device.stop()
+		self.device.close()
+		self.cap.release()
+
+	def kinect_close(self):
+		self.device.stop()
+		self.device.close()
+
+	def lifcam_close(self):
+		# When everything done, release the capture
+		self.cap.release()
+
+	def kinect_saver(self, prefix):
+		'''
+		Save the image and depth data captured by kinect camera.
+		Image saves in .jpg fomat, depth data saved as numpy array.
+
+		Args:
+			prefix: the prefix of the save files.
+		'''
+		frames = self.listener.waitForNewFrame()
+		color = frames["color"]
+		depth = frames["depth"]
+		color = cv2.resize(color.asarray(),
+		                           (int(1920 / 2), int(1080 / 2)))
+		color = cv2.flip(color, 1)
+		# save image in qhd size
+		cv2.imwrite(prefix + '_color.jpg', color)
+		# cv2.imwrite(prefix + '_depth.jpg', cv2.flip(depth.asarray(), 1))
+		np.save(prefix + '_depth.npy', cv2.flip(depth.asarray(), 1))
+		self.listener.release(frames)
+
+	def lifcam_saver(self, file_name):
+		'''
+		Save the image captured by Webcam.
+
+		Args:
+			file_name: the file name of the saving image.
+		'''
+		# Capture frame-by-frame
+		ret, frame = self.cap.read()
+		# Our operations on the frame come here
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+		# Display the resulting frame
+		cv2.imwrite(file_name, gray)
+		
+		
