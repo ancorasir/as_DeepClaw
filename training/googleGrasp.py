d88de1c79c6dc75f8efa2e7a3e9ac9ab9f31050d
Xiaoyi He <remi011@mail.ustc.edu.cn>
2017-04-12 17:16:23 +1000

    add CEM to grasping cycle

--- training/googleGrasp.py
+++ training/googleGrasp.py
@@ -201,7 +201,7 @@ def training(loss, learning_rate):
     # Add a scalar summary for the snapshot loss.
     #tf.scalar_summary(loss.op.name, loss)
     # Create the gradient descent optimizer with the given learning rate.
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    optimizer = tf.train.AdamOptimizer(learning_rate)
     # Create a variable to track the global step.
     global_step = tf.Variable(0, name='global_step', trainable=False)
     # Use the optimizer to apply the gradients that minimize the loss
