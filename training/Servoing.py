8a198c7dc8e17b61a6bea60e3ae18320e5fa7855
Xiaoyi He <remi011@mail.ustc.edu.cn>
2017-04-13 15:48:40 +1000

    change motion Tensor's shape to (1, 3)

--- training/Servoing.py
+++ training/Servoing.py
@@ -36,6 +36,7 @@ class Servoing(object):
         for iter in range(3):
             # sampling N grasp directions vt, shape = [N, 7]
             Xs = []
+            performance = []
             # for _ in range(N):
             i = 0
             while i < N:
@@ -46,19 +47,24 @@ class Servoing(object):
                     X[0][0] -= position[0] # x vector
                     X[0][1] -= position[1] # y vector
                     Xs.append(X[0])
+                    X = np.array(X, dtype=np.float32)
+                    perf = self.inference.eval(session=self.sess, feed_dict={
+                        self.image: images,
+                        self.motions: X,
+                        self.is_training: False
+                        }).T[0]
+                    
+                    performance.append(perf)
                     i += 1
             Xs = np.array(Xs, dtype=np.float32)
+            performance = np.array(performance)
 
             # select the 6 best grasp directions by inferring to the network
             # get images input from camera
             # load the trained network, ??need more work
             # performance = np.sum(Xs, axis=1) # for demonstration
             
-            performance = self.inference.eval(session=self.sess, feed_dict={
-                self.image: images,
-                self.motions: Xs,
-                self.is_training: False
-                }).T[0]
+            
 
             # Sort X by objective function values (in ascending order)
             best_idx = np.argsort(performance)[-M:]
@@ -72,24 +78,27 @@ class Servoing(object):
             #print(mean, cov)
         # use the optimized parameter to infer the best grasp direction
         Xs = []
+        performance = []
         i = 0
         while i < N:
             X = np.random.multivariate_normal(mean, cov, 1)
-            if -0.65 < X[0][0] < -0.32 and -0.2 < X[0][1] < 0.23 and -1.57 < X[0][-1] < 1.57:
-            #if (rotations<=180) and (gripper in workspace)
+            if -0.65 < X[0][0] < -0.32 and -0.2 < X[0][1] < 0.23 and -1.57 < X[0][2] < 1.57:
+            # make sure the sample grasp in within the workspace of the robotic gripper
+            #if (rotations<=180) and (gripper in workspace):
                 X[0][0] -= position[0] # x vector
                 X[0][1] -= position[1] # y vector
                 Xs.append(X[0])
+                X = np.array(X, dtype=np.float32)
+                perf = self.inference.eval(session=self.sess, feed_dict={
+                    self.image: images,
+                    self.motions: X,
+                    self.is_training: False
+                    }).T[0]
+                
+                performance.append(perf)
                 i += 1
-        Xs = np.array(Xs)
-        print(Xs)
-        # selecting the best grasp directions
-        performance = self.inference.eval(session=self.sess, feed_dict={
-                self.image: images,
-                self.motions: Xs,
-                self.is_training: False
-                }).T[0]
-        print(performance)
+        Xs = np.array(Xs, dtype=np.float32)
+        performance = np.array(performance)
         # performance = np.sum(Xs, axis=1)
 
         best_idx = np.argsort(performance)[-1:]
@@ -105,7 +114,6 @@ class Servoing(object):
         '''
         images = np.concatenate((image_00, image_01), axis=0).reshape((1, 472*2, 472, 3))
         grasp = np.array([0, 0, 0], dtype=np.float32)
-        grasp = np.array([grasp for _ in range(64)], dtype=np.float32)
 
         performance_grasp = self.inference.eval(session=self.sess, feed_dict={
                 self.image: images,
