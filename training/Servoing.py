e5b8d858baf83c3a8a5c1f06544cb7757f65add5
Ancora-SIRlab <sophie.fwan@gmail.com>
2017-04-19 19:15:21 +1000

    Rotation~(-pi,pi], random generate motion not position

--- training/Servoing.py
+++ training/Servoing.py
@@ -33,81 +33,57 @@ class Servoing(object):
         # position: (-0.65 ~ -0.32, -0.2~0.23)
         mean = [0, 0, 0]
         cov = np.diagflat([1, 1, 1])
-
         
         # google's work uses 3 iteration of optimization
         for iter in range(3):
-            # sampling N grasp directions vt, shape = [N, 7]
+            # sampling N grasp directions vt, shape = [N, 3]
             Xs = []
             performance = []
-            # for _ in range(N):
             i = 0
             while i < N:
                 X = np.random.multivariate_normal(mean, cov, 1)
-                if -0.65 < X[0][0] < -0.32 and -0.2 < X[0][1] < 0.23 and -1.57 < X[0][2] < 1.57:
-                # make sure the sample grasp in within the workspace of the robotic gripper
-                #if (rotations<=180) and (gripper in workspace):
-                    X[0][0] -= position[0] # x vector
-                    X[0][1] -= position[1] # y vector
+                if -0.65 < X[0][0]+position[0] < -0.32 and -0.2 < X[0][1]+position[1] < 0.23 and -3.14 < X[0][2] <= 3.14:
+                #if (-pi<rotations<=pi) and (gripper in workspace):
                     Xs.append(X[0])
                     X = np.array(X, dtype=np.float32)
-                    perf = self.inference.eval(session=self.sess, feed_dict={
+                    logits = self.inference.eval(session=self.sess, feed_dict={
                         self.image: images,
                         self.motions: X,
                         self.is_training: False
                         }).T[0][0]
-                    
-                    performance.append(perf)
+                    performance.append(self.sigmoid(logits))
                     i += 1
             Xs = np.array(Xs, dtype=np.float32)
             performance = np.array(performance)
-            print(Xs)
-            print(performance)
+	    # print the infered probability of first 64 ramdon vectors
+            if iter == 0:
+	    	print(Xs)
+            	print(performance)
 
             # select the 6 best grasp directions by inferring to the network
-            # get images input from camera
-            # load the trained network, ??need more work
-            # performance = np.sum(Xs, axis=1) # for demonstration
-            
-            
-
-            # Sort X by objective function values (in ascending order)
             best_idx = np.argsort(performance)[-M:]
             best_Xs = np.array(Xs)[best_idx,:]
-            #print(performance)
-            #print(best_idx)
-            #print(best_Xs)
             # Update parameters of distribution from the M best grasp directions
             mean = np.mean(best_Xs, axis=0)
-            mean[0] += position[0]
-            mean[1] += position[1]
             cov = np.cov(best_Xs, rowvar=0)
-            #print(mean, cov)
         # use the optimized parameter to infer the best grasp direction
         Xs = []
         performance = []
         i = 0
         while i < N:
             X = np.random.multivariate_normal(mean, cov, 1)
-            if -0.65 < X[0][0] < -0.32 and -0.2 < X[0][1] < 0.23 and -1.57 < X[0][2] < 1.57:
-            # make sure the sample grasp in within the workspace of the robotic gripper
-            #if (rotations<=180) and (gripper in workspace):
-                X[0][0] -= position[0] # x vector
-                X[0][1] -= position[1] # y vector
+            if -0.65 < X[0][0]+position[0] < -0.32 and -0.2 < X[0][1]+position[1] < 0.23 and -3.14 < X[0][2] < 3.14:
                 Xs.append(X[0])
                 X = np.array(X, dtype=np.float32)
-                perf = self.inference.eval(session=self.sess, feed_dict={
+                logits = self.inference.eval(session=self.sess, feed_dict={
                     self.image: images,
                     self.motions: X,
                     self.is_training: False
-                    }).T[0][0]
-                
-                performance.append(perf)
+                    })[0][0]
+                performance.append(self.sigmoid(logits))
                 i += 1
         Xs = np.array(Xs, dtype=np.float32)
         performance = np.array(performance)
-        #print(performance)
-        # performance = np.sum(Xs, axis=1)
 
         best_idx = np.argsort(performance)[-1:]
         position[0] += Xs[best_idx][0][0] 
